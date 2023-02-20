baa25ff8ea35323ddb9eb1e4d274521fde6c76ce
Mira <mira.theidel@gmx.de>
2023-02-20 10:00:01 +0100

    wip: connecting custom py-file

--- src/test.py
+++ src/test.py
@@ -1,2 +1,16 @@
+from sympy.parsing.sympy_parser import standard_transformations, convert_xor, implicit_multiplication, convert_equals_signs
+import sympy as smp
+import base64
+from io import BytesIO
+import matplotlib.pyplot as plt
+
+
 def test():
     return "Hello there"
+
+def second_func():
+    return test()
+
+
+if __name__ == "__main__":
+    second_func()
