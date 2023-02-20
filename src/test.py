cce09b0310d14839214d4065611966eaed8638ac
Mira <mira.theidel@gmx.de>
2023-02-20 10:34:21 +0100

    wip: connecting custom py-file

--- src/test.py
+++ src/test.py
@@ -1,8 +1,7 @@
-from sympy.parsing.sympy_parser import standard_transformations, convert_xor, implicit_multiplication, convert_equals_signs
-import sympy as smp
+
 import base64
 from io import BytesIO
-import matplotlib.pyplot as plt
+
 
 
 def test():
@@ -12,5 +11,3 @@ def second_func():
     return test()
 
 
-if __name__ == "__main__":
-    second_func()
